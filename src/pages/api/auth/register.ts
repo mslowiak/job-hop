539919e536bfd9b573f87646e0e2e3acfc2c20ce
Marcin Słowiak <m.slowiak@smartrecruiters.com>
2025-11-16 21:43:45 +0100

    Do proper redirect after sign up

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -12,6 +12,9 @@ export const POST: APIRoute = async ({ request, cookies }) => {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
+    options: {
+      emailRedirectTo: `${new URL(request.url).origin}/auth/login?confirmed=true`,
+    },
   });
 
   if (error) {
